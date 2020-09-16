8818a35c43ca701feceacc1426d6abd178a23d13
dannitay <dannielletaylor@outlook.com>
2020-09-15 20:42:23 -0500

    adding onclick function to square component to register user clicks

--- tictactoe/src/index.js
+++ tictactoe/src/index.js
@@ -3,15 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 
-class Square extends React.Component {
+    class Square extends React.Component {
     render() {
       return (
-        <button className="square">
-          {this.props.value}
-        </button>
-      );
+            <button className="square" onClick={() => { alert('click'); }}>
+             {this.props.value}
+            </button>
+            );
+        }
     }
-  }
   
   class Board extends React.Component {
     renderSquare(i) {
@@ -66,4 +66,3 @@ class Square extends React.Component {
     <Game />,
     document.getElementById('root')
   );
-  
