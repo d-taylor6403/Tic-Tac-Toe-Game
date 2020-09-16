a1449ebcfd61aacb042fccc1d825f4a255a5546e
dannitay <dannielletaylor@outlook.com>
2020-09-15 20:44:53 -0500

    adding state constructor to Square component

--- tictactoe/src/index.js
+++ tictactoe/src/index.js
@@ -4,8 +4,15 @@ import './index.css';
 
 
     class Square extends React.Component {
-    render() {
-      return (
+        constructor(props){
+            super(props);
+            this.state={
+                value: null,
+            };
+        }
+
+        render() {
+        return (
             <button className="square" onClick={() => { alert('click'); }}>
              {this.props.value}
             </button>
