18d258bd5e2c8f3fb72e846d6288008b13deb709
dannitay <dannielletaylor@outlook.com>
2020-09-15 21:10:20 -0500

    lifting state up to board component to be accesbile by all components

--- tictactoe/src/index.js
+++ tictactoe/src/index.js
@@ -21,6 +21,12 @@ import './index.css';
     }
   
   class Board extends React.Component {
+    constructor(props){
+        super(props);
+        this.state={
+            squares: Array(9).fill(null),
+        };
+    }
     renderSquare(i) {
       return <Square value={i} />;
     }
